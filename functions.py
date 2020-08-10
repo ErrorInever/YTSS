24f282aea2f10d458479f6bbcfa348246cfe71c4
ErrorInever <errorinever@gmail.com>
2020-08-10 18:17:44 +0000

    switch to youtube-dl

--- functions.py
+++ functions.py
@@ -1,47 +1,63 @@
-import pytube
+from __future__ import unicode_literals
 import sys
+import youtube_dl
 from termcolor import cprint
 from utils import show_progress_download
 from config.cfg import cfg
-from pydub import AudioSegment
 from utils import get_time_code
 
 
-def _save_audio(audio):
-    """
-    Saves audio to disk
-    :param audio: ``pytube.streams.Stream``
-    """
-    if not isinstance(audio, pytube.streams.Stream):
-        raise TypeError('expect {}, but get {}'.format(pytube.streams.Stream.__name__, type(audio)))
-    song_name = audio.default_filename
-    cprint('SONG NAME --- {}'.format(song_name), 'yellow',
-           attrs=['bold', 'underline', 'reverse'])
-
-    audio.download(output_path=cfg.OUT_DIR)
-    cprint('Complete: {}/{}'.format(cfg.OUT_DIR, song_name), 'green',
-           attrs=['bold', 'underline', 'reverse'])
+def download_hook(d):
+    if d['status'] == 'downloading':
+        print(d['filename'], d['_percent_str'])
+    elif d['status'] == 'finished':
+        print('Download is complete, now converting')
 
 
-def download_audio(url):
+def download_audio(url, out_dir):
     """
     Downloads audio from youtube video
     :param url: video url
+    :param out_dir: path to out directory
     """
-    youtub = pytube.YouTube(url, on_progress_callback=show_progress_download)
-    audio = youtub.streams.filter(only_audio=True).all()
-    _save_audio(audio[0])
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': out_dir + '/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'progress_hooks': [download_hook],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
+def download_playlist(url, out_dir, **kwargs):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': out_dir + '/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'progress_hooks': [download_hook],
+    }
 
-def download_playlist(url):
-    """
-    Downloads playlist from youtube
-    :param url: playlist url
-    """
-    playlist = pytube.Playlist(url)
-    for video_url in playlist.video_urls:
-        download_audio(video_url)
+    for key in kwargs:
+        ydl_opts[key] = kwargs[key]
 
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == '__main__':
+    # args = {
+    #     'playliststart': 3,
+    #     'playlistend': 4,
+    # }
+    #args = {'playlist_items': '1, 2'}
+    download_playlist('https://www.youtube.com/playlist?list=PLKvVP6uqHnAb600O3C0Di8Ntjd3tnT_hh', '~/some', **args)
 
 # def cut_sound(time_code, path_to_audio):
 #     """
